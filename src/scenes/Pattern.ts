ccc10df22a3047406e6c559b5be2aa2825724a22
twsau <contact.twsau@gmail.com>
2023-01-02 10:04:06 +0000

    single iteration in pattern points constructor

--- src/scenes/Pattern.ts
+++ src/scenes/Pattern.ts
@@ -9,8 +9,11 @@ export class PatternScene extends Container implements IScene {
     super();
     this.state = { count: 10000 };
     this.position.set(window.innerWidth / 2, window.innerHeight / 2);
-    this.points = Array.from(Array(100)).map(() => new Graphics());
-    this.points.forEach((point) => this.addChild(point));
+    this.points = Array.from(Array(100)).map(() => {
+      const graphic = new Graphics();
+      this.addChild(graphic);
+      return graphic;
+    });
   }
 
   update(_delta: number) {
@@ -19,11 +22,14 @@ export class PatternScene extends Container implements IScene {
       const point = this.points[i];
       point.clear();
       point
-        .beginFill(0x00ffff, 0.5 + Math.abs(Math.cos(i * this.state.count / 5000)) * 0.5)
+        .beginFill(
+          0x00ffff,
+          0.5 + Math.abs(Math.cos((i * this.state.count) / 5000)) * 0.5
+        )
         .drawCircle(
           i * Math.cos((i * this.state.count) / 10000) * 5,
           i * Math.sin((i * this.state.count) / 10000) * 5,
-          3 * Math.abs(Math.cos(i * this.state.count / 500))
+          3 * Math.abs(Math.cos((i * this.state.count) / 500))
         )
         .endFill();
     }
