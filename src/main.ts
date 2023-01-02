6214cbe9a632ac6eb0052c3585eb6d6ccdfe5620
twsau <contact.twsau@gmail.com>
2023-01-02 10:05:08 +0000

    tidy up main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { Game } from "./Game";
 import { LoadingScene } from "./scenes/Loading";
 
-const main = async () => {
-  Game.initialize({ antialias: true });
-  Game.loadScene(new LoadingScene());
-};
-
-main();
+Game.initialize({ antialias: true });
+Game.loadScene(new LoadingScene());
